afc8cd8f5d2ba74be5c7bb0f548534814cc91f7b
amitpaultl <amitpaultl@gmail.com>
2022-09-29 14:01:55 +0600

    Some design correction

--- src/componts/Item/Item.js
+++ src/componts/Item/Item.js
@@ -16,8 +16,8 @@ const Item = (props) => {
                 <div className='card-body'>
                     <h5 className='card-title'>{name}</h5>
                     <p className='card-text'>{about}</p>
-                    <p>For Age: {age} </p>
-                    <p>Time required :{time}seconds</p>
+                    <p className='age-time'>For Age: <span>{age}</span>  </p>
+                    <p className='age-time'>Time required : <span>{time} seconds</span></p>
                     <button onClick={()=> clickAdd(props.card.time)} href="#" className='btn btn-primary w-100'>Add to List</button>
                 </div>
             </div>
